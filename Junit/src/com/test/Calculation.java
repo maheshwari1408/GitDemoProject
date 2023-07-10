9c97c4235ef3124bb8d8cdfdcbf902fbecc10531
maheshwarikatta <maheshwarikatta@maheshwaris-air>
2023-07-10 17:20:12 -0500

    project is created

---
+++ Junit/src/com/test/Calculation.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Calculation {
+	public static int addNumbers(int a, int b)
+	{
+		int c=a+b;
+		return c;
+	}
+
+}
---
+++ Junit/src/com/test/TestAdd.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdd {
+
+	@Test
+	public void addNumbers() {
+		assertEquals(10,Calculation.addNumbers(5,5));
+	}
+	}
+
